c4f6cbc8bd133d23b8e26153799ee222deefe8c5
vzrenggamani <rengga.prakoso@gmail.com>
2024-11-20 02:40:26 +0700

    fix: add ref to a tag

--- app/header.tsx
+++ app/header.tsx
@@ -181,11 +181,12 @@ export function Header() {
 const ListItem = React.forwardRef<
   React.ElementRef<'a'>,
   React.ComponentPropsWithoutRef<'a'>
->(({ className, title, children, ...props }) => {
+>(({ className, title, children, ...props }, ref) => {
   return (
     <li>
       <NavigationMenuLink asChild>
         <a
+          ref={ref}
           className={cn(
             'block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground',
             className,
